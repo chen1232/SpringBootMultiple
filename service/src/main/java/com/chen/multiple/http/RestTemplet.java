88312d89d451dcee82e254d5847a72b2b8c4ea4b
chen <1657751029@qq.com>
2020-03-08 12:14:49 +0800

    增加resttemplet

--- service/src/main/java/com/chen/multiple/http/RestTemplet.java
+++ service/src/main/java/com/chen/multiple/http/RestTemplet.java
@@ -1,8 +1,12 @@
 package com.chen.multiple.http;
 
 import model.UserInfo;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -17,7 +21,18 @@ public class RestTemplet {
     }
 
     public UserInfo get() {
-        ResponseEntity<UserInfo> userInfoResponseEntity = restTemplate.postForEntity("http://localhost:8080/multiple/sys/v1/user/getUserInfo", "1", UserInfo.class);
+
+        //headers
+        HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.add("api-version", "1.0");
+        //body
+        MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
+        requestBody.add("roundid", "1");
+        //HttpEntity
+        HttpEntity<MultiValueMap> requestEntity = new HttpEntity<MultiValueMap>(requestBody, requestHeaders);
+
+        ResponseEntity<UserInfo> userInfoResponseEntity =
+                restTemplate.postForEntity("http://localhost:8080/multiple/sys/v1/user/getUserInfo", requestEntity, UserInfo.class);
         UserInfo body = userInfoResponseEntity.getBody();
         return body;
     }
